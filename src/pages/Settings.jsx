dcf2efbf8f105117473431cc96575959a9c2c767
anuragkainth <anuragkainth07@gmail.com>
2026-04-13 23:26:05 +0530

    feat: added settings and user pages

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+function Settings() {
+    return (
+      <div>
+        <h2 className="text-2xl font-semibold">Settings Page</h2>
+      </div>
+    );
+  }
+  
+  export default Settings;
---
+++ src/pages/Users.jsx
@@ -0,0 +1,9 @@
+function Users() {
+    return (
+      <div>
+        <h2 className="text-2xl font-semibold">Users Page</h2>
+      </div>
+    );
+  }
+  
+  export default Users;
