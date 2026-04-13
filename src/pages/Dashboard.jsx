c0d94e2d528dcb17c3f26479147edfbdf34c81fd
anuragkainth <anuragkainth07@gmail.com>
2026-04-13 23:23:54 +0530

    chore: useEffect for API data fetch & code refactoring

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,20 +1,29 @@
 import StatCard from "../components/StatCard";
 import DataTable from "../components/DataTable";
 import BarChartComponent from "../components/BarChartComponent";
+import { useEffect, useState } from "react";
+import { fetchStats, fetchUsers } from "../services/api";
 
 function Dashboard() {
-  const stats = [
-    { title: "Revenue", value: "$24,000", change: 12 },
-    { title: "Users", value: "1,200", change: 5 },
-    { title: "Orders", value: "320", change: -2 },
-    { title: "Growth", value: "18%", change: 8 },
-  ];
+
+  const [stats, setStats] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const statsData = await fetchStats();
+      const usersData = await fetchUsers();
+      
+      setStats(statsData);
+      setUsers(usersData);
+      setLoading(false);
+    };
+  
+    loadData();
+  }, []);
+
   const columns = ["name", "email", "role"];
-  const users = [
-    { name: "Anurag", email: "anurag@mail.com", role: "Admin" },
-    { name: "Rahul", email: "rahul@mail.com", role: "User" },
-    { name: "Sneha", email: "sneha@mail.com", role: "Editor" },
-  ];
   const chartData = [
     { name: "Jan", sales: 400 },
     { name: "Feb", sales: 300 },
@@ -22,10 +31,29 @@ function Dashboard() {
     { name: "Apr", sales: 200 },
   ];
 
+  if (loading) {
+    return (
+      <div className="p-6">
+        <div className="animate-pulse space-y-4">
+          {/* Cards skeleton */}
+          <div className="grid grid-cols-4 gap-4">
+            <div className="h-20 bg-gray-300 rounded"></div>
+            <div className="h-20 bg-gray-300 rounded"></div>
+            <div className="h-20 bg-gray-300 rounded"></div>
+            <div className="h-20 bg-gray-300 rounded"></div>
+          </div>
+          {/* Chart skeleton */}
+          <div className="h-64 bg-gray-300 rounded"></div>
+          {/* Table skeleton */}
+          <div className="h-40 bg-gray-300 rounded"></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2 className="text-2xl font-semibold mb-4">Dashboard</h2>
-
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {stats.map((item, index) => (
           <StatCard
@@ -36,8 +64,8 @@ function Dashboard() {
           />
         ))}
       </div>
-      <DataTable columns={columns} data={users} />
       <BarChartComponent data={chartData} />
+      <DataTable columns={columns} data={users} />
     </div>
   );
 }
